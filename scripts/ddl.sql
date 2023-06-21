e49c4590debe67a501dbf07fa8d415966ec3d841
MU_SALEH <MU@git.com>
2023-06-21 19:18:19 +0300

    create the ddl.sql file

---
+++ scripts/ddl.sql
@@ -0,0 +1,32 @@
+-- DROP SCHEMA bms;
+
+CREATE SCHEMA bms ;
+
+
+CREATE TABLE book (
+	book_id serial4 NOT NULL,
+	book_title varchar(300) NOT NULL,
+	book_description varchar(1000) NULL,
+	book_auther varchar(50) NOT NULL,
+	book_publisher varchar(50) NOT NULL,
+	book_pages int4 NULL,
+	store_code varchar(5) NOT NULL,
+	created_on timestamp NOT NULL,
+	created_by varchar(50) NOT NULL,
+	CONSTRAINT book_pkey PRIMARY KEY (book_id)
+);
+
+-- bms.store definition
+
+-- Drop table
+
+-- DROP TABLE store;
+
+CREATE TABLE store (
+	store_id serial4 NOT NULL,
+	store_name varchar(100) NOT NULL,
+	store_code varchar(5) NULL,
+	created_on timestamp NOT NULL,
+	created_by varchar(50) NOT NULL,
+	CONSTRAINT store_pkey PRIMARY KEY (store_id)
+);
